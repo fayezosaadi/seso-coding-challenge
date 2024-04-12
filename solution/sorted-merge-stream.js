b7ceb3f8e39161d3912d731fcddea5f5e7bf00e6
Fayez Saadi <fayezosaadi@gmail.com>
2024-04-12 14:36:18 -0700

    Add comments to SortedMergeStream

--- solution/sorted-merge-stream.js
+++ solution/sorted-merge-stream.js
@@ -1,18 +1,27 @@
 const {Writable} = require('stream');
 
+/**
+ * SortedMergeStream class represents a writable stream that buffers and sorts log entries
+ * before emitting them for external consumption. It ensures that log entries are emitted
+ * in chronological order.
+ *
+ * **/
 module.exports = class SortedMergeStream extends Writable {
   constructor() {
     super({objectMode: true});
+
+    // Initialize an empty buffer to store log entries
     this.buffer = [];
   }
 
   _write(chunk, encoding, callback) {
     this.buffer.push(chunk);
+    // Signal that the write operation is complete
     callback();
   }
 
   _final(callback) {
-    // Sort the buffer
+    // Sort the buffer in ascending order based on the date property of each log entry
     this.buffer.sort((a, b) => a.date - b.date)
 
     // Emit each log entry for external consumption
@@ -22,6 +31,8 @@ module.exports = class SortedMergeStream extends Writable {
 
     // Signal the end of data
     this.emit('end');
+
+    // Signal that the final operation is complete
     callback();
   }
 }
